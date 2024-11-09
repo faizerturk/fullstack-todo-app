8b1940fd859e3b1861b6e016e9a2d3f3ff88917b
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-11-09 15:28:23 +0300

    .

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,16 +1,16 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import Home from '../pages/index';
+import Home from '../app/page';
 
 describe('Todo App', () => {
   it('should add a new todo item', () => {
     render(<Home />);
-    
+
     const input = screen.getByPlaceholderText('Yeni bir görev ekleyin');
     const addButton = screen.getByText('Ekle');
-    
+
     fireEvent.change(input, { target: { value: 'buy some milk' } });
     fireEvent.click(addButton);
-    
+
     const todoItem = screen.getByText('buy some milk');
     expect(todoItem).toBeInTheDocument();
   });
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
 };
