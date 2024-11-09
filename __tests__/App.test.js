63601663047dd56e07d56dab964465a8c81193cc
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-11-09 15:22:04 +0300

    jest
    jest

---
+++ __tests__/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Home from '../pages/index';
+
+describe('Todo App', () => {
+  it('should add a new todo item', () => {
+    render(<Home />);
+    
+    const input = screen.getByPlaceholderText('Yeni bir görev ekleyin');
+    const addButton = screen.getByText('Ekle');
+    
+    fireEvent.change(input, { target: { value: 'buy some milk' } });
+    fireEvent.click(addButton);
+    
+    const todoItem = screen.getByText('buy some milk');
+    expect(todoItem).toBeInTheDocument();
+  });
+});
